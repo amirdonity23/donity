aa9ffaa1c80db2942cf8188cba5bacca54f196aa
amirdonity23 <58296090+amirdonity23@users.noreply.github.com>
2023-09-16 21:37:46 +0200

    Update and rename amirdonity88.js to amirdonity888.js

--- amirdonity88.js
+++
@@ -1,2 +0,0 @@
-$(` <link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=sky-bold" /> <link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera" /> <link rel="stylesheet" type="text/css" href="https://rawcdn.githack.com/amirdonity23/donity/2dc602ad3c66d9397a76a7070c15147ec99f8047/amirdonity88.css
-"> `).insertBefore('body'); 
---
+++ amirdonity888.js
@@ -0,0 +1,2 @@
+$(` <link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=sky-bold" /> <link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera" /> <link rel="stylesheet" type="text/css" href="https://rawcdn.githack.com/amirdonity23/donity/79d1106d92555c5f03d01ef1c8e1805e6d53c566/amirdonity888.css
+"> `).insertBefore('body'); 
